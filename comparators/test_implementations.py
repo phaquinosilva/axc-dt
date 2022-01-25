f83c5c086e05cf087e03bb5f434bd6e8a5a3434a
Pedro Aquino <phaquino1998@gmail.com>
2022-01-25 14:44:34 -0300

    Start test suite
    * late inclusion of a simple test

--- comparators/test_implementations.py
+++ comparators/test_implementations.py
@@ -1,10 +1,10 @@
 import pytest
 from four_bit.analysis.axdc import axdc2, axdc6, edc
-from nbit.analysis.nbit_comparators import n_adc2, n_adc6, n_edc
+from n_bit.analysis.nbit_comparators import n_axdc2, n_axdc6, n_edc
 
 
 @pytest.mark.parametrize(
-    "original, n_bit", [(edc, n_edc), (axdc2, n_adc2), (axdc6, n_adc6)]
+    "original, n_bit", [(edc, n_edc), (axdc2, n_axdc2), (axdc6, n_axdc6)]
 )
 def test_4bit_comparators(original, n_bit):
     diff_counter = 0
